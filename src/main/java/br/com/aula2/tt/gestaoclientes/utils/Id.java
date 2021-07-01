0e2f1a8dce754a422aa213d50085a4108e043b5e
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-01 17:57:34 -0300

    minor fix

--- src/main/java/br/com/aula2/tt/gestaoclientes/utils/Id.java
+++ src/main/java/br/com/aula2/tt/gestaoclientes/utils/Id.java
@@ -1,9 +1,19 @@
 package br.com.aula2.tt.gestaoclientes.utils;
 
 public class Id {
-    private static long id = 1l;
+    private static long idCliente = 1l;
+    private static long idProduto = 1l;
+    private static long idPedido = 1l;
 
-    public static long generateID() {
-        return id++;
+    public static long generateIDCliente() {
+        return idCliente++;
+    }
+
+    public static long generateIDProduto() {
+        return idProduto++;
+    }
+
+    public static long generateIDPedido() {
+        return idPedido++;
     }
 }
