b497b16bc56c03ef46feaf1edee0f0841da66f00
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-01 17:34:29 -0300

    Inicialização do projeto spring boot

---
+++ README.md
@@ -0,0 +1,5 @@
+# Aula 2 - Estender métodos web e JSON - TT
+
+## Prática Complementar - Exercício 2 - Gestão de Clientes
+
+Respositório que contém a solução para o exercício 2 da aula 2 TT.
---
+++ src/test/java/br/com/aula2/tt/gestaoclientes/GestaoClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aula2.tt.gestaoclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoClientesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
