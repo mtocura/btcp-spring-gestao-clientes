9bece2d1c28dab290daab27d0948c6ab7d174d68
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-01 17:51:36 -0300

    adicionando o atributo de lista de pedidos

--- src/main/java/br/com/aula2/tt/gestaoclientes/entities/Cliente.java
+++ src/main/java/br/com/aula2/tt/gestaoclientes/entities/Cliente.java
@@ -1,18 +1,22 @@
 package br.com.aula2.tt.gestaoclientes.entities;
 
+import java.util.List;
+
 public class Cliente {
     private Long id;
     private String nome;
     private String cpf;
     private String email;
     private String telefone;
+    private List<Pedido> pedidos;
 
-    public Cliente(Long id, String nome, String cpf, String email, String telefone) {
+    public Cliente(Long id, String nome, String cpf, String email, String telefone, List<Pedido> pedidos) {
         this.id = id;
         this.nome = nome;
         this.cpf = cpf;
         this.email = email;
         this.telefone = telefone;
+        this.pedidos = pedidos;
     }
 
     public Long getId() {
@@ -53,5 +57,13 @@ public class Cliente {
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
     }
 }
