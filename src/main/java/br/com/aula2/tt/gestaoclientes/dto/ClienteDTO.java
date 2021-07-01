4121dc1eef12ca5bc2ea722ea0779c589827517b
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-01 17:53:34 -0300

    criação do dto para cliente

---
+++ src/main/java/br/com/aula2/tt/gestaoclientes/dto/ClienteDTO.java
@@ -0,0 +1,71 @@
+package br.com.aula2.tt.gestaoclientes.dto;
+
+import br.com.aula2.tt.gestaoclientes.entities.Pedido;
+
+import java.util.List;
+
+public class ClienteDTO {
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private List<Pedido> pedidos;
+
+    public ClienteDTO(Long id, String nome, String cpf, String email, String telefone, List<Pedido> pedidos) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.pedidos = pedidos;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+}
