565368f60fa89800fcad8c5b1b1c7e6fb1499cea
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-02 07:33:01 -0300

    criação do DAO de clientes

---
+++ src/main/java/br/com/aula2/tt/gestaoclientes/dao/ClienteDAO.java
@@ -0,0 +1,65 @@
+package br.com.aula2.tt.gestaoclientes.dao;
+
+import br.com.aula2.tt.gestaoclientes.dto.ClienteDTO;
+import br.com.aula2.tt.gestaoclientes.entities.Cliente;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ClienteDAO {
+    private static List<Cliente> clientes = new ArrayList<>();
+
+    public static Cliente clienteDtoToEntity(ClienteDTO clienteDTO) {
+        return new Cliente(
+                (long) clientes.size() + 1,
+                clienteDTO.getNome(),
+                clienteDTO.getCpf(),
+                clienteDTO.getEmail(),
+                clienteDTO.getTelefone(),
+                clienteDTO.getPedidos()
+        );
+    }
+
+    public static ClienteDTO entityToDTO(Cliente cliente) {
+        return new ClienteDTO(
+                cliente.getCpf(),
+                cliente.getNome(),
+                cliente.getEmail(),
+                cliente.getTelefone(),
+                cliente.getPedidos()
+        );
+    }
+
+    public static List<ClienteDTO> entityToDTO(List<Cliente> cliente) {
+        List<ClienteDTO> clienteDTOS = new ArrayList<>();
+
+        cliente.forEach(c -> clienteDTOS.add(entityToDTO(c)));
+
+        return clienteDTOS;
+    }
+
+    public void addCliente(ClienteDTO clienteDTO) {
+        clientes.add(clienteDtoToEntity(clienteDTO));
+    }
+
+    public ClienteDTO pedidos(String cpf) {
+        Optional<Cliente> clienteOptional = clientes.stream().filter(cliente -> cliente.getCpf().equals(cpf)).findAny();
+
+        if(clienteOptional.isPresent()) {
+            return entityToDTO(clienteOptional.get());
+        }
+
+        return null;
+    }
+
+    public ClienteDTO pedidos(long id) {
+        Optional<Cliente> clienteOptional = clientes.stream().filter(cliente -> cliente.getId() == id).findAny();
+
+        if(clienteOptional.isPresent()) {
+            return entityToDTO(clienteOptional.get());
+        }
+
+        return null;
+    }
+}
