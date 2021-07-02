793196c0e3ccd43feca0b5444c69cdec2f5563f4
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-02 07:43:54 -0300

    criação do controller

---
+++ src/main/java/br/com/aula2/tt/gestaoclientes/controller/ClienteController.java
@@ -0,0 +1,53 @@
+package br.com.aula2.tt.gestaoclientes.controller;
+
+import br.com.aula2.tt.gestaoclientes.dao.ClienteDAO;
+import br.com.aula2.tt.gestaoclientes.dto.ClienteDTO;
+import br.com.aula2.tt.gestaoclientes.entities.Cliente;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/aula2/tt")
+public class ClienteController {
+
+    private ClienteDAO clienteDAO = new ClienteDAO();
+
+    @GetMapping("/clientes")
+    public ResponseEntity<?> getAllClients() {
+        return ResponseEntity.ok(clienteDAO.findAll());
+    }
+
+    @GetMapping("/clientes/{id}")
+    public ResponseEntity<?> getPedidosClienteById(@PathVariable long id) {
+        ClienteDTO cliente = clienteDAO.findClient(id);
+
+        if(cliente != null) {
+            return ResponseEntity.ok(cliente);
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/clientes/{cpf}")
+    public ResponseEntity<?> getPedidosClienteById(@PathVariable String cpf) {
+        ClienteDTO cliente = clienteDAO.findClient(cpf);
+
+        if(cliente != null) {
+            return ResponseEntity.ok(cliente);
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("/clientes")
+    public ResponseEntity<?> createClient(@RequestBody ClienteDTO clienteDTO, UriComponentsBuilder uriBuilder) {
+        Cliente cliente = clienteDAO.addCliente(clienteDTO);
+
+        URI uri = uriBuilder.path("/aula2/tt/clientes/{id}").buildAndExpand(cliente.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+}
