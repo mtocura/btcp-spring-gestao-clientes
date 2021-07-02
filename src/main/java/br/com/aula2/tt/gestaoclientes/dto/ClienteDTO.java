443a95fca6676ce7a3ca50ea4190750a7c18f3a8
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-02 07:27:31 -0300

    Removendo id do DTO

--- src/main/java/br/com/aula2/tt/gestaoclientes/dto/ClienteDTO.java
+++ src/main/java/br/com/aula2/tt/gestaoclientes/dto/ClienteDTO.java
@@ -5,28 +5,18 @@ import br.com.aula2.tt.gestaoclientes.entities.Pedido;
 import java.util.List;
 
 public class ClienteDTO {
-    private Long id;
     private String nome;
     private String cpf;
     private String email;
     private String telefone;
     private List<Pedido> pedidos;
 
-    public ClienteDTO(Long id, String nome, String cpf, String email, String telefone, List<Pedido> pedidos) {
-        this.id = id;
+    public ClienteDTO(String nome, String cpf, String email, String telefone, List<Pedido> pedidos) {
         this.nome = nome;
         this.cpf = cpf;
         this.email = email;
         this.telefone = telefone;
         this.pedidos = pedidos;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getNome() {
